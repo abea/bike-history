a8c786f1f9da496ad8c7287b84e5c6281d66460e
Alex Bea <alex.bea@gmail.com>
2018-09-02 13:44:39 -0400

    Sets up test for station saving.

--- test/test.js
+++ test/test.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
-const Weather = require('../models/Weather');
+// const {expect} = require('chai');
+
+// const Weather = require('../models/Weather');
 // const StationDay = require('../models/StationDay');
 require('dotenv').config({ path: `${__dirname}/../variables.env` });
 
-describe('Weather', function() {
+describe('Adding stations', function() {
   // Before starting the test, create a sandboxed database connection
-  before(function (done) {
+  before((done) => {
     mongoose.connect('mongodb://localhost/tests');
     const db = mongoose.connection;
 
     db.on('error', (err) => { done(err); });
 
     db.once('open', () => { done(); });
+  });
+
+  describe('Station save', function() {
+    it('should fail if missing the kioskId field', async () => {
+      // Hit route with bad station data as req.body.
+      // Route processes the data, trying to save the station update.
+      // Data processing fails because there's no kioskId.
+
+      // const stationData = {
+      //   addressStreet: '1168 E. Passyunk',
+      //   addressCity: 'Philadelphia',
+      //   addressState: 'PA',
+      //   addressZipCode: '19147'
+      // };
+
+      // expect(saver).to.throw();
+    });
   });
 
   // After all tests are finished drop database and close connection
