01089fa9d3d1c06b4bd4f45a6aa1c3552ab3827e
Alex Bea <alex.bea@gmail.com>
2018-09-09 15:10:44 -0400

    Outlines tests to write

--- test/get-data.js
+++ test/get-data.js
@@ -3,8 +3,9 @@ const {expect} = require('chai');
 require('dotenv').config({ path: `${__dirname}/../variables.env` });
 
 const weatherUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const bikesUrl = 'https://www.rideindego.com/stations/json';
 
-describe('Query', function() {
+describe('Open Weather', function() {
   it('should receive a response from the weather API with key.', async () => {
     const reqOptions = {
       uri: `${weatherUrl}?appid=${process.env.WEATHER_KEY}&id=${process.env.WEATHER_CITY_ID}`,
@@ -22,6 +23,26 @@ describe('Query', function() {
 
     expect(result.cod).to.equal(200);
   });
+});
 
-  // If sending weather for a date that hasn't been created, we should get a null result back.
+describe('Weather Post', function() {
+  it('should receive a document with _id back from post request.', async () => {
+    expect(result._id).to.exist();
+  });
+
+  it('should have data for the hour when sent.', async () => {
+    expect(result.hours[hour].name).to.exist();
+  });
+
+});
+
+describe('Stations Post', function() {
+  it('should receive a report of number of stations updated.', async () => {
+    expect(typeof result).to.equal('string'); // Or use regex
+  });
+
+  it('should have data on the docs for the hour sent.', async () => {
+    // Mongo query for a random doc.
+    expect(result.hours[hour].timestamp).to.exist();
+  });
 });
