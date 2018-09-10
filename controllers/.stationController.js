4e476f5ad97e34af7ce9e463b6a5fb5624b41ec4
Alex Bea <alex.bea@gmail.com>
2018-09-09 21:24:57 -0400

    Deletes unused file

--- controllers/.stationController.js
+++
@@ -1,9 +0,0 @@
-// ## Notes for station controller
-//  - Receive data throught a post route
-//  - Async series over the stations in the results.
-//  - Add the timestamp to each station status.
-//  - If there's a document for that station and day, update the StationDay
-//    document with the hour's snapshot.
-//  - If not, create a document for the station and day, adding empty objects
-//    for the other hours in the day and populate the first hour, including the
-//    saved timestamp.
