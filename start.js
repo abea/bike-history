1b0c3951d07cdfec8784f279ba61a7f1ab23f3f8
Alex Bea <abea@punkave.com>
2018-09-23 20:43:41 -0400

    Adds home page (#1)

--- start.js
+++ start.js
@@ -21,5 +21,5 @@ const app = require('./app');
 app.set('port', process.env.PORT || 7777);
 
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running on port ${server.address().port}`);
+  console.info(`Express running on port ${server.address().port}`);
 });
