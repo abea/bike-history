d31364a12ca374bd1cd4f7c35234caef4e07bba1
Alex Bea <alex.bea@gmail.com>
2018-10-29 19:28:06 -0400

    Enables CORS

--- server/app.js
+++ server/app.js
@@ -9,6 +9,16 @@ app.set('port', process.env.PORT || 7777);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  // NOTE: https://enable-cors.org/server_expressjs.html
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/', routes);
 
 module.exports = app;
