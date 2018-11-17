b2d9a4327fed59955588f39960edb437d1abc716
Alex Bea <alex.bea@gmail.com>
2018-11-17 13:21:30 -0500

    Moves chart component into Analytic wrapper

---
+++ src/components/Analytic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <Chart mode="getOneSnap"/>
+  </div>
+</template>
+
+<script>
+import Chart from '@/components/Chart.vue';
+
+export default {
+  name: 'Analytic',
+  components: {
+    Chart
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <main class="row justify-content-center">
-    <Chart mode="getOneSnap" class="col-sm-8 col-md-6"/>
-    <Chart mode="getOneSnap" class="col-sm-8 col-md-6"/>
+    <Analytic class="col-sm-8 col-md-6"/>
+    <Analytic class="col-sm-8 col-md-6"/>
   </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import Chart from '@/components/Chart.vue';
+import Analytic from '@/components/Analytic.vue';
 
 export default {
   name: 'home',
   components: {
-    Chart
+    Analytic
   }
 };
 </script>
