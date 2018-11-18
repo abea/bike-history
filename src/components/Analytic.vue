7254c60ec6e1ffc0e41338f987b8c37511ca3040
Alex Bea <alex.bea@gmail.com>
2018-11-18 11:29:55 -0500

    Moves station watching into Analytic

--- src/components/Analytic.vue
+++ src/components/Analytic.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <Chart mode="getOneSnap" :toDate="toDate" :toTime="toTime"/>
+    <Chart :station="station"/>
     <form >
       <label for="startDate">
         Snapshot date
@@ -8,15 +8,16 @@
       </label>
       <label for="startTime">
         Snapshot time
-        <input type="time" name="startTime">
+        <input type="time" name="startTime" v-model="toTime">
       </label>
       <input type="submit" value="Submit">
     </form>
-    <p>Date: {{toDate}} | Time: {{toTime}}</p>
+    <p>Date: {{ toDate || 'Not set' }} | Time: {{ toTime || 'Not set' }}</p>
   </div>
 </template>
 
 <script>
+import ApiService from '@/services/ApiService';
 import Chart from '@/components/Chart.vue';
 import moment from 'moment';
 
@@ -34,9 +35,59 @@ export default {
   },
   data() {
     return {
+      stationId: 3069,
       toDate: initialDate,
-      toTime: initialTime
+      toTime: initialTime,
+      station: {},
+      mode: 'getOneSnap'
     };
+  },
+  methods: {
+    getInfo: async function(opts) {
+      const stationData = await ApiService[this.mode]({
+        id: opts.id,
+        toTime: `${opts.toDate}T${opts.toTime}`,
+        fromTime: `${opts.fromDate}T${opts.fromTime}`
+      })
+        .then(res => {
+          // TODO deal with the different results from other request modes.
+          return res.data.station.properties;
+        })
+        .catch(err => {
+          console.error('🚨', err);
+          return {};
+        });
+
+      return stationData;
+    }
+  },
+  mounted: async function() {
+    this.$watch(
+      () => ({
+        mode: this.mode,
+        stationId: this.stationId,
+        toDate: this.toDate,
+        toTime: this.toTime
+      }),
+      async function(data) {
+        this.station = await this.getInfo({
+          mode: data.mode,
+          id: data.stationId,
+          toDate: data.toDate ? data.toDate : initialDate,
+          toTime: data.toTime ? data.toTime : initialTime
+          // fromDate: this.mode === 'getOneSeries' ? this.fromDate : null,
+          // fromTime: this.mode === 'getOneSeries' ? this.fromTime : null
+        });
+      }
+    );
+
+    // Get the real station data to start.
+    this.station = await this.getInfo({
+      mode: this.mode,
+      id: this.stationId,
+      toDate: this.toDate,
+      toTime: this.toTime
+    });
   }
 };
 </script>
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,20 +1,14 @@
 <script>
-import ApiService from '@/services/ApiService';
 import { Pie } from 'vue-chartjs';
 
 export default {
   extends: Pie,
   name: 'Chart',
   props: {
-    mode: String,
-    toDate: String,
-    toTime: String
-  },
-  data() {
-    return {
-      stationId: 3069,
-      station: {}
-    };
+    station: {
+      type: Object,
+      required: true
+    }
   },
   watch: {
     station: function() {
@@ -32,53 +26,6 @@ export default {
         {}
       );
     }
-  },
-  methods: {
-    getInfo: async function(opts) {
-      const stationData = await ApiService[this.mode]({
-        id: opts.id,
-        toTime: `${opts.toDate}T${opts.toTime}`,
-        fromTime: `${opts.fromDate}T${opts.fromTime}`
-      })
-        .then(res => {
-          // TODO deal with the different results from other request modes.
-          return res.data.station.properties;
-        })
-        .catch(err => {
-          console.error('🚨', err);
-          return {};
-        });
-
-      return stationData;
-    }
-  },
-  mounted: async function() {
-    this.$watch(
-      () => ({
-        mode: this.mode,
-        stationId: this.stationId,
-        toDate: this.toDate,
-        toTime: this.toTime
-      }),
-      async function(data) {
-        this.station = await this.getInfo({
-          mode: data.mode,
-          id: data.stationId,
-          toDate: data.toDate,
-          toTime: data.toTime
-          // fromDate: this.mode === 'getOneSeries' ? this.fromDate : null,
-          // fromTime: this.mode === 'getOneSeries' ? this.fromTime : null
-        });
-      }
-    );
-
-    // Get the real station data to start.
-    this.station = await this.getInfo({
-      mode: this.mode,
-      id: this.stationId,
-      toDate: this.toDate,
-      toTime: this.toTime
-    });
   }
 };
 </script>
